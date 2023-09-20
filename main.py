6887faf5e95d721e795818f5dc25f18a2df21be3
GTRows <aciroglu.fatih@gmail.com>
2023-09-20 07:17:52 +0300

    Add print statements

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 from Twitter import Twitter
 from Twitch import Twitch
 import time
+import datetime
 
 
 def main():
+    print("Starting...")
     twitter = Twitter()
     twitch = Twitch()
 
     while True:
         messages = twitch.check_channels()
+        print(f"Found {len(messages)} messages to tweet!")
         for msg in messages:
+            print(f"Sending tweet: {msg}")
             twitter.send_tweet(msg)
+        print(f"Checked at {datetime.datetime.now()}")
         time.sleep(60)
+
+    # This will never be printed,
+    print("Done!")
 
 
 if __name__ == "__main__":
