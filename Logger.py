aac62183be3a94f941ad45254f636b742244ac69
GTRows <aciroglu.fatih@gmail.com>
2023-09-21 15:54:51 +0300

    Introduce CHECK_INTERVAL for periodic Twitch checks and enhance logging.

---
+++ Logger.py
@@ -0,0 +1,23 @@
+import logging
+
+
+class Logger:
+
+    def __init__(self, name):
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(logging.DEBUG)
+
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+        file_handler = logging.FileHandler('app.log')
+        file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(formatter)
+
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(formatter)
+
+        self.logger.addHandler(file_handler)
+        self.logger.addHandler(stream_handler)
+
+    def get_logger(self):
+        return self.logger
--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 from Twitter import Twitter
 from Twitch import Twitch
+from Logger import Logger
 import time
 import datetime
 
+log = Logger(__name__).get_logger()
+
 
 def main():
+    check_interval = 60
     print("Starting...")
+    log.info("Starting...")
     twitter = Twitter()
     twitch = Twitch()
 
     while True:
         messages = twitch.check_channels()
-        print(f"Found {len(messages)} messages to tweet!")
+        if len(messages) > 0:
+            log.info(f"Found {len(messages)} messages to tweet!")
+        else:
+            log.info("No messages to tweet!")
         for msg in messages:
-            print(f"Sending tweet: {msg}")
+            log.info(f"Sending tweet: {msg}")
             twitter.send_tweet(msg)
-        print(f"Checked at {datetime.datetime.now()}")
-        time.sleep(60)
+        log.info(f"We're going to sleep for {check_interval} seconds...")
+        time.sleep(check_interval)
 
-    # This will never be printed,
-    print("Done!")
+    # This will never be printed
+    log.error("Something went wrong!")
 
 
 if __name__ == "__main__":
